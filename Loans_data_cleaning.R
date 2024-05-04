5575538b412b089f73f124a6317e8fb2008a6448
Jacob Majakwara <148438487+jmajakwara@users.noreply.github.com>
2024-05-04 15:47:34 +0200

    R codes for cleaning the data

---
+++ Loans_data_cleaning.R
@@ -0,0 +1,188 @@
+##---- Cleaning ---------------------------------------------------------
+
+library(tidyverse)
+library(caret)
+library(data.table)
+library(survival)
+
+
+loans.data <-  read.csv("Data\\DataProsper.csv", header = TRUE, sep = ",", na.strings="NA")
+loans.data <- read.csv("Data\\DataProsper.csv", header = TRUE, sep = ",", na.strings="NA")
+dim(loans.data)
+str(loans.data)
+## Converting a factor variable which is recorded as integer
+loans.data[, which(colnames(loans.data) == "Term")]<-as.factor(loans.data$Term)
+loans.data[, which(colnames(loans.data) == "ProsperRating..numeric.")]<-as.factor(loans.data$ProsperRating..numeric.)
+#table(loans.data$LoanStatus, useNA = "always")
+table(loans.data$ProsperScore, useNA = "always")
+dim(loans.data)
+max(loans.data$LoanOriginationDate)
+
+
+## Removing duplicates
+loan_nd<-loans.data[!duplicated(loans.data$LoanKey),]
+
+
+## creating status variable for censoring
+loans<-loan_nd %>% mutate(status = ifelse(loan_nd$LoanStatus=="Defaulted" | loan_nd$LoanStatus=="Chargedoff" |
+                loan_nd$LoanStatus=="Past Due (>120 days)",1,0)) 
+
+loans[, which(colnames(loans) == "status")]<-as.factor(loans$status)
+
+table(loans$status)
+## adding the final date to Closed Date variable
+loans$ClosedDate <- as.Date(loans$ClosedDate, format = "%Y-%m-%d")
+loans$ClosedDate[is.na(loans$ClosedDate)] <- as.Date("2014-11-03")
+
+
+## creating the time-to-event variable
+loans$start<-as.Date(loans$LoanOriginationDate)
+loans$end<-as.Date(loans$ClosedDate)
+loans$time<-as.numeric(difftime(loans$end,loans$start,units="days"))
+#table(filter(loans, time<0)$time)
+#length(filter(loans, time<0)$time)
+#table(filter(loans, time<0)$start)
+#table(filter(loans, time<0)$end)
+loans<-loans[-loans$time < 0,] #so that there is no time less than 0
+
+
+## Converting date variables to be in date format
+loans[,which(colnames(loans) == "ListingCreationDate")]<-as.Date(loans$ListingCreationDate)
+loans[,which(colnames(loans) == "LoanOriginationDate")]<-as.Date(loans$LoanOriginationDate)
+loans[,which(colnames(loans) == "ClosedDate")]<-as.Date(loans$ClosedDate)
+loans[,which(colnames(loans) == "FirstRecordedCreditLine")]<-as.Date(loans$FirstRecordedCreditLine)
+
+
+## removing variables that have nothing to do with PD
+# LoanKey and ListingKey maintained for identification when imputing
+id_var<-c(which(colnames(loans)=="ListingNumber"),which(colnames(loans)=="ListingCreationDate"),
+          which(colnames(loans)=="ClosedDate"),which(colnames(loans)=="GroupKey"),which(colnames(loans)=="DateCreditPulled"),
+          which(colnames(loans) =="LoanFirstDefaultedCycleNumber"),which(colnames(loans)=="LoanNumber"),
+          which(colnames(loans)=="MemberKey"),which(colnames(loans)=="start"),which(colnames(loans)=="end"))
+
+loans<-select(loans,-all_of(id_var))  
+# can also use loans<-loans[,-id_var]
+
+loans <- mutate_if(loans, is.character, as.factor)
+
+
+## Dealing with loans from "2009-08-01"
+loans_filtered<-filter(loans, LoanOriginationDate > as.Date("2009-07-31"))
+#dim(loans_filtered)
+#min(loans_filtered$LoanOriginationDate)
+
+
+## Replacing zeros in Income variable with NA for those Employed and Full-time on Employment Status and DebtToIncomeRatio
+#table(filter(loans_filtered,StatedMonthlyIncome==0)$EmploymentStatus)
+
+loans_filtered$StatedMonthlyIncome<-ifelse(loans_filtered$StatedMonthlyIncome==0 & loans_filtered$EmploymentStatus=="Employed" | 
+         loans_filtered$StatedMonthlyIncome==0 & loans_filtered$EmploymentStatus=="Full-time", NA,loans_filtered$StatedMonthlyIncome)
+
+loans_filtered$DebtToIncomeRatio <- ifelse(loans_filtered$DebtToIncomeRatio==0, NA,loans_filtered$DebtToIncomeRatio)
+
+
+#table(filter(loans_filtered,is.na(StatedMonthlyIncome))$EmploymentStatus)
+
+## Calculating percentage of missingness
+
+# sapply(loans_filtered1, function(x) sum(is.na(x))/n*100) this can also be used
+missingness <- colMeans(is.na(loans_filtered))*100
+#sort(missingness)
+
+
+## variables with high missingness > 76%
+high_miss<-c("TotalProsperLoans","TotalProsperPaymentsBilled", "OnTimeProsperPayments" ,"ProsperPaymentsLessThanOneMonthLate",
+            "ProsperPaymentsOneMonthPlusLate", "ProsperPrincipalBorrowed", "ProsperPrincipalOutstanding",
+            "ScorexChangeAtTimeOfListing", "CreditGrade")
+
+## Variables that have same information
+rep_var<-c("FirstRecordedCreditLine","IncomeRange","LoanOriginationDate","LoanStatus",
+           "LoanOriginationQuarter", "ProsperRating..Alpha.")
+
+
+## Removing variables with high missingness >50% and same information
+loans_final<-select(loans_filtered,-all_of(c(high_miss,rep_var)))  
+
+loans_final <- loans_final %>% rename(c("ProsperRating" = "ProsperRating..numeric.",
+                                        "ListingCategory" = "ListingCategory..numeric.",
+                                        "TradesNeverDelinquent" = "TradesNeverDelinquent..percentage."))
+## Combining credit scores lower and upper range into  credit score range
+loans_final <- loans_final %>% 
+                 unite(CreditScoreRange,CreditScoreRangeLower,CreditScoreRangeUpper,sep = "-")
+
+loans_final[,which(colnames(loans_final) == "CreditScoreRange")]<-as.factor(loans_final$CreditScoreRange)
+
+table(loans_final$CreditScoreRange)
+# categorising credit range into Poor, Fair, Good, Very Good and Excellent
+
+loans_final <- loans_final %>% 
+  mutate(
+    CreditScoreRange=case_match(CreditScoreRange,c('600-619','620-639','640-659') ~ "Fair",
+                                c('660-679','680-699','700-719','720-739') ~ "Good", c('740-759','760-779','780-799') ~ "Very good",
+                                c('800-819','820-839','840-859','860-879','880-899') ~ "Excellent")
+         ) 
+
+loans_final$CreditScoreRange  <- factor(as.factor(loans_final$CreditScoreRange),ordered = TRUE,
+                                    levels=c("Fair","Good","Very good","Excellent"))
+
+str(loans_final)
+## Arranging variables
+var <- c("LoanKey","ListingKey","time","status","BorrowerRate","LenderYield","ProsperScore","EmploymentStatusDuration",
+         "CurrentCreditLines","OpenCreditLines","TotalCreditLinespast7years","OpenRevolvingAccounts",
+         "OpenRevolvingMonthlyPayment","InquiriesLast6Months","TotalInquiries","RevolvingCreditBalance",
+         "CurrentDelinquencies", "AmountDelinquent","DelinquenciesLast7Years","PublicRecordsLast10Years",
+         "PublicRecordsLast12Months","BankcardUtilization","AvailableBankcardCredit","DebtToIncomeRatio",
+         "LoanOriginalAmount","PercentFunded","StatedMonthlyIncome","TradesNeverDelinquent",
+         "LoanCurrentDaysDelinquent","IncomeVerifiable","ProsperRating","ListingCategory","EmploymentStatus",
+         "Occupation","CurrentlyInGroup","IsBorrowerHomeowner","CreditScoreRange","Term")
+length(var)
+loans_final <- loans_final %>% select(all_of(var),everything())
+
+## Final data to be used for analysis
+write.csv(loans_final,file="C:\\Users\\Jacob\\OneDrive - University of Witwatersrand\\RESEARCH\\Credit Risk Modeling\\Imputation\\Data\\loans_final_jj1.csv", row.names = FALSE)
+str(loans_final)
+
+## Using Loans when Term==36 months
+loans36<-filter(loans_final, Term=="36")
+n36=dim(loans36)[1]
+missing36<- summarise_all(loans36,~sum(is.na(.))/n36*100 -> m36)
+#table(filter(loans36,StatedMonthlyIncome<1160)$EmploymentStatus)
+
+sort(missing36)
+dim(loans36)
+str(loans36)
+
+## Finding Pearson correlation for numerical variables
+loans_36 <- loans36 %>% select(where(is.numeric))
+loans36_complete_cases <- filter(loans_36,complete.cases(loans_36)==TRUE) #CC
+Corr_CC <- cor(loans36_complete_cases) #calculating correlations for continuous complete cases
+highly_correlated <- findCorrelation(Corr_CC, cutoff = 0.9, names = TRUE,exact=FALSE) # function in caret package
+highly_correlated
+
+
+# Function to check variables with higher correlations
+corr_check <- function(dataset, threshold = 0.7) {
+  
+          # Calculate the correlation matrix
+        cor_matrix <- cor(dataset)
+  
+          # Find the indices of the highly correlated variables
+        high_cor <- which(abs(cor_matrix) > threshold & upper.tri(cor_matrix, diag = FALSE), arr.ind = TRUE)
+  
+          # Print the names of the highly correlated variables
+        if (length(high_cor) > 0) {
+              cat("The following variables have correlations higher than the threshold:\n")
+            for (i in 1:nrow(high_cor)) {
+               cat(paste0(" . ", rownames(cor_matrix)[high_cor[i, 1]], " and ", colnames(cor_matrix)[high_cor[i, 2]], "\n"))
+                                        }
+                                   } else {
+                                    cat("No variables have correlations higher than the threshold.")
+                                           }
+                                                    }
+
+corr_check(loans36_complete_cases,0.9)
+
+
+
+
+
---
+++ Multicollinearity.R
@@ -0,0 +1,196 @@
+#### Multicollinearity #####
+library(MASS)
+library(tidyverse)
+library(caret)
+library(data.table)
+library(survival)
+library(dplyr)
+library(ggplot2)
+library(ggpubr)
+library(survminer)
+library(rsample)
+library(flexsurv)
+library(tidymodels)
+library(censored)
+library(prodlim)
+loans <- read.table("C:\\Users\\a0025724\\OneDrive - University of Witwatersrand\\RESEARCH\\Credit Risk Modeling\\Imputation\\Data\\loans_final_jj.csv",header=TRUE,sep=",",na.strings = "NA")
+#loans <- read.csv("Data/loans_final_jj.csv",header=TRUE,sep=",",na.strings = "NA")
+loans$time <- as.numeric(loans$time)
+loans$OpenRevolvingMonthlyPayment <- as.numeric(loans$OpenRevolvingMonthlyPayment)
+loans$EmploymentStatusDuration <- as.numeric(loans$EmploymentStatusDuration)
+loans$AmountDelinquent <- as.numeric(loans$AmountDelinquent)
+loans$OpenRevolvingMonthlyPayment <- as.numeric(loans$OpenRevolvingMonthlyPayment)
+loans$RevolvingCreditBalance <- as.numeric(loans$RevolvingCreditBalance)
+loans$AvailableBankcardCredit <- as.numeric(loans$AvailableBankcardCredit)
+loans$LoanOriginalAmount <- as.numeric(loans$LoanOriginalAmount)
+loans$ListingCategory <- as.factor(loans$ListingCategory)
+loans$EmploymentStatus <- as.factor(loans$EmploymentStatus)
+loans$CreditScoreRange <- as.factor(loans$CreditScoreRange)
+loans$Occupation <- as.factor(loans$Occupation)
+
+
+# convert days to months
+#loans <- loans %>% mutate(month = round(time/30.4375, digit=0))
+
+#term_adj_fctn <- function(df, t) {
+#  term  <-  df$Term == t
+#  df <- df[term, ]
+  # plot frequncy graph
+  #barplot(table(df$month) / sum(table(df$month)))
+  
+#  maxtime <- t
+#  max_term  <-  df$month <= maxtime
+#  df <- df[max_term, ]
+#}
+
+
+#loans_12m <- term_adj_fctn(loans, 12)
+#loans_36m <- term_adj_fctn(loans, 36)
+#loans_60m <- term_adj_fctn(loans, 60)
+
+# combine adjusted datasets
+#loans <- rbind(loans_12m, loans_36m, loans_60m)
+#loans$time <- round(loans$time/30.417,0)
+
+# Removing cases whose debt to income ratio is more than 100
+loans <- loans %>% filter(DebtToIncomeRatio < 1 | is.na(DebtToIncomeRatio))
+
+loans <- loans %>% dplyr::select(-2,-Occupation,-ProsperScore)
+
+#loans <- loans %>% dplyr::select(time : MonthlyLoanPayment)
+
+dim(loans)
+# loans <- loans %>% mutate_if(is.character, as.factor)
+# loans %>%  summarize_all(funs(sum(is.na(.))))/dim(loans)[1]*100
+
+loans_cc <- loans %>% na.omit()
+
+# Scaling the numerical variables using caret package
+
+
+
+#train_index <- createDataPartition(loans_cc$status,p=0.7,list=FALSE)
+#loans_train <- loans_cc[train_index,]
+#loans_test <- loans_cc[-train_index,]
+
+
+
+# Split the data into a test and training set
+# following https://www.tidymodels.org/start/recipes/#recipe
+set.seed(1974)
+library(rsample)
+data_split <- initial_split(loans_cc, prop = 0.70, strata = status)     
+#Create data frames for the two sets:
+loans_train <- training(data_split)
+loans_test  <- testing(data_split)
+
+loans_train_all <- loans[!(loans$LoanKey %in% c(loans_test$LoanKey)),]
+
+#loans_train_all <- loans_train_all %>% select(-1)
+
+loans_train <- loans_train %>% dplyr::select(time : MonthlyLoanPayment)
+loans_test <- loans_test %>% dplyr::select(time : MonthlyLoanPayment)
+
+## Partioning of data with missingnes
+loans_missing <- loans[!(loans$LoanKey %in% c(loans_cc$LoanKey)),]
+dim(loans_cc)
+dim(loans_missing)
+## Variables aliased (Highly correlated > 0.5)
+##  LenderYield and BorrowRate; OppenCreditLines and CurrentCreditLines;OpenRevolvingAccounts and Current CreditLines;
+##   ProsperScore and ProsperRating; BorrowerRate and BorrowerAPR, TradesNeverDelinquent and DelinquenciesLast7Years
+##   CurrentlyInGroup and IsBorrowerHomeowner,OpenRevolvingAccounts and OpenRevolvingMonthlyPayment#"ProsperScore",
+highly <- loans_cc %>% 
+            dplyr::select("LenderYield","BorrowerRate","OpenCreditLines","CurrentCreditLines","OpenRevolvingAccounts","BorrowerAPR","OpenRevolvingAccounts","ProsperRating","DebtToIncomeRatio","StatedMonthlyIncome","EmploymentStatusDuration","InquiriesLast6Months","TotalInquiries","AmountDelinquent","DelinquenciesLast7Years","CurrentDelinquencies") %>%  cor()
+highly 
+
+#After removing variables highly correlated; PropserRating, CurrentCreditLines, LenderYield ,"OpenRevolvingAccounts","TotalInquiries","ProsperScore",
+
+var_sel <- c("time","status","BorrowerRate","EmploymentStatusDuration","OpenCreditLines",
+        "TotalCreditLinespast7years","InquiriesLast6Months","Term","AmountDelinquent","TradesOpenedLast6Months","DelinquenciesLast7Years", "RevolvingCreditBalance","PublicRecordsLast10Years", "BankcardUtilization","CurrentlyInGroup","OpenRevolvingAccounts", "DebtToIncomeRatio","LoanOriginalAmount","TradesNeverDelinquent","PercentFunded", "EmploymentStatus", 
+       "LoanCurrentDaysDelinquent","IsBorrowerHomeowner","CreditScoreRange","CurrentDelinquencies","StatedMonthlyIncome")
+
+loans_train <- loans_train %>% dplyr::select(all_of(var_sel))
+
+
+
+# fit survival model
+library(survival)
+cox_model <- coxph(Surv(time, status) ~ ., data = loans_train, y=TRUE, x = TRUE)
+
+# calculate VIF for each predictor variable; VIF greater than 5 indicates significant multicollinearity.
+library(car)
+cat("calculate VIF for each predictor variable, might need to consider those VIF > 2.5")
+vif(cox_model)
+
+
+## # perform PCA on predictor variables
+
+#cat("Performing PCA on scaled predictor numerical variables")
+#pca <- prcomp(loans_train[, c(3:16)], center = TRUE, scale = TRUE)
+#summary(pca)
+
+#plot(pca, type = "l", ylim=c(0,4) )
+#abline(h=0)
+#abline(v=0)
+
+#pca$rotation[,1:7]
+
+#table(loans_cc$InquiriesLast6Months_cat)
+loans_cc <- loans_cc |> mutate(InquiriesLast6Months_cat = ifelse(InquiriesLast6Months == 0, 0,
+                                              ifelse(InquiriesLast6Months == 1 | InquiriesLast6Months == 2,1,2)))
+
+
+fit.surv <- survfit(Surv(time, status) ~ InquiriesLast6Months_cat, data = loans_cc)
+ggsurvplot(fit.surv, conf.int = FALSE, 
+           xlab = "Time (in days)",
+           ylab = "Survival probability",
+           linetype =  "strata",
+           surv.plot.height = 1.2,
+         #  palette = "jco",
+           ggtheme =  theme_light(),
+           legend = "right",
+           legend.title = "Credit score ranges",
+           linewidth = 0.4,
+           size = 1)
+
+
+summary(survfit(Surv(time, status) ~ 1, data = loans_cc), times = c(1,365, median(loans_cc$time), 3*365, max(loans_cc$time)))
+
+
+
+### Fitting lognormal to data
+hist(loans_cc$BorrowerAPR)
+fit <- fitdistr(loans_cc$BorrowerAPR, "lognormal")
+fit$estimate
+fit$sd
+
+hist((loans_cc[loans_cc$status==1])$time)
+obs <- dplyr::filter(loans_cc,status==1)
+cens <- dplyr::filter(loans_cc,status==0)
+dim(loans_cc)
+dim(obs)
+dim(cens)
+hist(obs$time)
+hist(cens$time)
+
+## Fiting lognormal distribution to cases experience the event of interest
+fit <- fitdistr(obs$time, "lognormal")
+fit$estimate
+fit$sd
+
+fit1 <- fitdistr(cens$time, "lognormal")
+fit1$estimate
+fit1$sd
+
+# Fitting DTIR
+hist(loans_cc$CurrentDelinquencies)
+fit <- fitdistr(loans_cc$CurrentDelinquencies +0.00000001, "lognormal")
+fit$estimate
+fit$sd
+
+fit1 <- fitdistr(loans_cc$DebtToIncomeRatio, "lognormal")
+fit1$estimate
+fit1$sd
+
+table((loans_cc$Term)[loans_cc$Term==36])
+
